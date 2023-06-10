5fa1ca5b478ed610673edb92fb573fc14c7a1409
Viverino1 <Vivek.maddineni@gmail.com>
2023-06-10 13:06:20 -0500

    empty chassis class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpProgram.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpProgram.java
@@ -6,16 +6,29 @@ import org.firstinspires.ftc.teamcode.VergeTools.Motor;
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp (name = "DriverMode")
 public class TeleOpProgram extends OpMode{
-    Chassis chassis = new Chassis(hardwareMap);
-
+    Motor frontRight;
+    Motor frontLeft;
+    Motor backRight;
+    Motor backLeft;
+    int myVar;
     @Override
     public void init(){
-
+        frontRight = new Motor("frontright", hardwareMap);
+        frontLeft = new Motor("frontleft", hardwareMap);
+        backRight = new Motor("backright", hardwareMap);
+        backLeft = new Motor("backleft", hardwareMap);
     }
 
     @Override
     public void loop(){
-        chassis.x.move(gamepad1.left_stick_x);
-        chassis.y.move(gamepad1.left_stick_y);
+        frontLeft.move(-gamepad1.right_stick_y + gamepad1.right_stick_x);
+        backLeft.move(-gamepad1.right_stick_y - gamepad1.right_stick_x);
+        frontRight.move(gamepad1.right_stick_y + gamepad1.right_stick_x);
+        backRight.move(gamepad1.right_stick_y - gamepad1.right_stick_x);
+
+        frontLeft.move(gamepad1.left_stick_x);
+        backLeft.move(gamepad1.left_stick_x);
+        frontRight.move(gamepad1.left_stick_x);
+        backRight.move(gamepad1.left_stick_x);
     }
 }
