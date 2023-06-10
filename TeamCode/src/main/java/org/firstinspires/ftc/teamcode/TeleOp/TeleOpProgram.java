bfdc69a54a57c6841d82a53014f5870e90c3d442
Viverino1 <Vivek.maddineni@gmail.com>
2023-06-10 15:50:11 -0500

    Fixed Movement

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpProgram.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TeleOpProgram.java
@@ -6,29 +6,31 @@ import org.firstinspires.ftc.teamcode.VergeTools.Motor;
 
 @com.qualcomm.robotcore.eventloop.opmode.TeleOp (name = "DriverMode")
 public class TeleOpProgram extends OpMode{
-    Motor frontRight;
-    Motor frontLeft;
-    Motor backRight;
-    Motor backLeft;
+    Chassis chassis;
     int myVar;
     @Override
     public void init(){
-        frontRight = new Motor("frontright", hardwareMap);
-        frontLeft = new Motor("frontleft", hardwareMap);
-        backRight = new Motor("backright", hardwareMap);
-        backLeft = new Motor("backleft", hardwareMap);
+        chassis = new Chassis(hardwareMap);
     }
 
     @Override
     public void loop(){
-        frontLeft.move(-gamepad1.right_stick_y + gamepad1.right_stick_x);
-        backLeft.move(-gamepad1.right_stick_y - gamepad1.right_stick_x);
-        frontRight.move(gamepad1.right_stick_y + gamepad1.right_stick_x);
-        backRight.move(gamepad1.right_stick_y - gamepad1.right_stick_x);
-
-        frontLeft.move(gamepad1.left_stick_x);
-        backLeft.move(gamepad1.left_stick_x);
-        frontRight.move(gamepad1.left_stick_x);
-        backRight.move(gamepad1.left_stick_x);
+        chassis.move(gamepad1.right_stick_x, gamepad1.right_stick_y);
+        chassis.rotate(gamepad1.left_stick_x);
+        if(gamepad1.right_bumper){
+            chassis.limit(0.4f);
+        }else{
+            chassis.limit(1);
+        }
+        if(gamepad1.dpad_up){
+            chassis.move(0, -1);
+        }else if(gamepad1.dpad_down){
+            chassis.move(0, 1);
+        }
+        if(gamepad1.dpad_right){
+            chassis.move(1, 0);
+        }else if(gamepad1.dpad_left){
+            chassis.move(-1, 0);
+        }
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VergeTools/Chassis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VergeTools/Chassis.java
@@ -3,9 +3,36 @@ package org.firstinspires.ftc.teamcode.VergeTools;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Chassis {
+    String[] motorNames = {"frontright", "backright", "frontleft", "backleft"};
     Motor[] motors = new Motor[4];
-    String[] motorNames = {"frontright", "frontleft", "backright", "backleft"};
-    void Chassis(HardwareMap hardwareMap){
+    public Chassis(HardwareMap hardwareMap){
+        for (int i = 0; i < motors.length; i++) {
+            motors[i] = new Motor(motorNames[i], hardwareMap);
+        }
+    }
 
+    public void move(float x, float y){
+        motors[0].move(x + y);
+        motors[1].move(-x + y);
+        motors[2].move(x - y);
+        motors[3].move(-x - y);
+    }
+
+    public void rotate(float amount){
+        for (int i = 0; i < motors.length; i++) {
+            motors[i].move(amount);
+        }
+    }
+
+    public void limit(float amount){
+        for (int i = 0; i < motors.length; i++) {
+            motors[i].mapLimit(amount);
+        }
+    }
+
+    public void stop(){
+        for (int i = 0; i < motors.length; i++) {
+            motors[i].stop();
+        }
     }
 }
